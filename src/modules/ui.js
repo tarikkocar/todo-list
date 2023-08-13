28442c53543ec832c3131253f1c2b9c1dffe4f26
ahmetkocar <ahmetkocar@gmail.com>
2023-08-14 02:48:32 +0300

    Fix issue with icons not being displayed

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -3,6 +3,7 @@ import Project from "./projects.js";
 import Task from "./tasks.js";
 import { isToday, isThisWeek, format } from "date-fns";
 import Edit from "../images/edit.svg";
+import html from "../../dist/index.html";
 
 export default class UI {
   constructor() {
